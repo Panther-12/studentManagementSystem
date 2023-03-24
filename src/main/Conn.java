13a7cda97ab0651bb458a3ca5e5d5fc6d69611af
Panther-12 <nimrodwalwe@gmail.com>
2023-03-24 23:13:03 +0300

    first commit

---
+++ src/main/Conn.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+import java.sql.*;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Conn {
+    Connection c;
+    Statement s;
+    
+    public Conn(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///studentManagementSystem","nimrod","12@17y017g3sa?");
+            s = c.createStatement();
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+        
+}
